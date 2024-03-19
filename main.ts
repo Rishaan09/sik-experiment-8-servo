3388787c172960797de31f053e22fcd8a1bc0dd9
Rishaan09 <163979914+Rishaan09@users.noreply.github.com>
2024-03-19 18:21:16 +1100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-let direction = 1
 let Angle = 0
+let direction = 1
 pins.servoWritePin(AnalogPin.P0, Angle)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     Angle += direction
     pins.servoWritePin(AnalogPin.P0, Angle)
     if (Angle >= 180 || Angle <= 0) {
         direction = direction * -1
     }
-    
 })
